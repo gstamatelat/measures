681a635aa7c1a73394102ae2e44ceec6439eb9d0
Giorgos Stamatelatos <gstamatelat@gmail.com>
2019-08-12 18:59:14 +0300

    Add MutualInformation

---
+++ src/main/java/gr/james/measures/MutualInformation.java
@@ -0,0 +1,85 @@
+package gr.james.measures;
+
+import java.util.Set;
+
+/**
+ * Normalized mutual information implementation.
+ */
+public class MutualInformation<T> {
+    private final double value;
+
+    /**
+     * Create a new {@link MutualInformation} from the given sets.
+     * <p>
+     * The {@code world} set must be a superset of {@code a} and {@code b} and this constructor will make no checks to
+     * ensure that.
+     *
+     * @param a     one set
+     * @param b     the other set
+     * @param world the world set
+     * @throws NullPointerException     if either {@code a} or {@code b} is {@code null}
+     * @throws IllegalArgumentException if either {@code a} or {@code b} is empty
+     */
+    public MutualInformation(Set<T> a, Set<T> b, Set<T> world) {
+        if (a.isEmpty() || b.isEmpty()) {
+            throw new IllegalArgumentException("Inputs cannot be empty");
+        }
+        Set<T> big;
+        Set<T> small;
+        if (a.size() > b.size()) {
+            big = a;
+            small = b;
+        } else {
+            big = b;
+            small = a;
+        }
+        int intersection = 0;
+        int union = big.size();
+        for (T t : small) {
+            if (big.contains(t)) {
+                intersection++;
+            } else {
+                union++;
+            }
+        }
+
+        final double N11 = intersection;
+        final double N00 = world.size() - union;
+        final double N10 = a.size() - intersection;
+        final double N01 = b.size() - intersection;
+        final double N0X = world.size() - a.size();
+        final double NX0 = world.size() - b.size();
+        final double NX1 = b.size();
+        final double N1X = a.size();
+        final double N = world.size();
+
+        final double pmi1 = (1.0 * N11 / N) * Math.log((1.0 * N * N11) / (1.0 * N1X * NX1)) / Math.log(2);
+        final double pmi2 = (1.0 * N01 / N) * Math.log((1.0 * N * N01) / (1.0 * N0X * NX1)) / Math.log(2);
+        final double pmi3 = (1.0 * N10 / N) * Math.log((1.0 * N * N10) / (1.0 * N1X * NX0)) / Math.log(2);
+        final double pmi4 = (1.0 * N00 / N) * Math.log((1.0 * N * N00) / (1.0 * N0X * NX0)) / Math.log(2);
+
+        final double entropy1 = (1.0 * N0X / N) * Math.log(1.0 * N0X / N) / Math.log(2) +
+                (1.0 * N1X / N) * Math.log(1.0 * N1X / N) / Math.log(2);
+
+        final double entropy2 = (1.0 * NX0 / N) * Math.log(1.0 * NX0 / N) / Math.log(2) +
+                (1.0 * NX1 / N) * Math.log(1.0 * NX1 / N) / Math.log(2);
+
+        final double mi = (Double.isNaN(pmi1) ? 0.0 : pmi1) +
+                (Double.isNaN(pmi2) ? 0.0 : pmi2) +
+                (Double.isNaN(pmi3) ? 0.0 : pmi3) +
+                (Double.isNaN(pmi4) ? 0.0 : pmi4);
+
+        this.value = -(2 * mi) / (entropy1 + entropy2);
+
+        assert this.value >= 0 - 1e-4 && this.value <= 1 + 1e-4;
+    }
+
+    /**
+     * Returns the Normalized mutual information of the inputs that this instance was created from.
+     *
+     * @return the Normalized mutual information of the inputs that this instance was created from
+     */
+    public double value() {
+        return this.value;
+    }
+}
---
+++ src/test/java/gr/james/measures/MutualInformationTests.java
@@ -0,0 +1,32 @@
+package gr.james.measures;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MutualInformationTests {
+    /**
+     * Must be 1 with same inputs.
+     */
+    @Test
+    public void identity() {
+        final Set<Integer> a = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+        final Set<Integer> b = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+        final Set<Integer> world = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
+        Assert.assertEquals(1.0, new MutualInformation<>(a, b, world).value(), 1e-8);
+    }
+
+    /**
+     * Commutativity.
+     */
+    @Test
+    public void commutativity() {
+        final Set<Integer> a = new HashSet<>(Arrays.asList(1, 2));
+        final Set<Integer> b = new HashSet<>(Arrays.asList(2, 3, 4));
+        final Set<Integer> world = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Assert.assertEquals(new MutualInformation<>(b, a, world).value(), new MutualInformation<>(a, b, world).value(), 1e-8);
+    }
+}
